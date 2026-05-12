6f87fb0d00ff161b46f98b89ce66ed25dbe01542
rbohra47 <59145774+rbohra47@users.noreply.github.com>
2026-05-12 16:26:02 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,7 @@
-def call(String Project, String ImageTag, String DockerhubUser){
+def call(String Project, String ImageTag, String DockerHubUser){
   withCredentials([usernamePassword(credentialsId: 'DockerHubCred', passwordVariable: 'DockerHubPass', usernameVariable: 'DockerHubUser' )])
   {
     sh "docker login -u ${DockerHubUser} -p ${DockerHubPass}"
-   }
-sh "docker push ${DockerHubUser}/${Project}:${ImageTag}"
+    sh "docker push ${DockerHubUser}/${Project}:${ImageTag}"
 }
-                                    
+}
